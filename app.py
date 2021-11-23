0543010d4b6b9e625baec8f165336406ab5f5d71
dkthecoder <dhruv.151@live.co.uk>
2021-11-23 23:20:15 +0000

    added black card, white card routes, generating local SQL databse for SQLite. setting up html css and layout.html for template inheritence/

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 from flask import Flask, url_for, request, render_template, session
+import os
+import db
+
 
 app = Flask(__name__)
+if __name__ == '__main__':
+    app.run(debug = True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
 
+
+db.init_app(app)
+
+#landing page
 @app.route("/")
 def index():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
 
+
+#add black card
+@app.route("/add_black_card")
+def add_black_card():
+    return "<p>black card add</p>"
+
+
+#add white card
+@app.route("/add_white_card")
+def add_white_card():
+    return "<p>white card add</p>"
