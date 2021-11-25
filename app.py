ff80fa10ccae7df3a14fdd27a14bd34a8c0aad86
dkthecoder <dhruv.151@live.co.uk>
2021-11-25 00:36:43 +0000

    developed logic for card retrieval, number randomization and adjusted templates for all routes. Rewritten magicmaker.py to perform its task easier.

--- app.py
+++ app.py
@@ -16,26 +16,77 @@ app.config['SECRET_KEY'] = '66ea3dab727dfa20322ca91c32854073'
 @app.route("/", methods=['POST', 'GET'])
 def index():
     form = LetsPlay()
-    #if form.validate_on_submit():
-
-
+    if form.validate_on_submit():
+        return redirect(url_for('play', given_word=form.word.data))
+       
     return render_template("index.html", title="welcome", form=form)
+
+
+#play
+#TAKES A INPUT
+@app.route('/play', defaults={'given_word': None}, methods=['POST', 'GET'])
+@app.route('/play/<given_word>', methods=['POST', 'GET'])
+def play(given_word):
+    form = LetsPlay()
+    num_of_bc = blackcards.length()
+    num_of_wc = whitecards.length()
+
+    if given_word == "feeling_lucky_punk":
+        bc = magicmaker.random_number_generator(0, num_of_bc, 1)
+        wc = magicmaker.random_number_generator(0, num_of_wc, 10)
+
+        bc_return = blackcards.retrieve_card(bc[0])
+        wc_return = []
+
+        counter = 0
+        for i in range(5):
+            temp = []
+            for j in range(2):
+                temp.append(whitecards.retrieve_card(wc[counter]))
+                counter = counter + 1
+            wc_return.append(temp)
+        return render_template("play.html", title="play", form=form, blackcard=bc_return, whitecards=wc_return)
+
+    else:
+        if form.validate_on_submit():
+            bc = magicmaker.rand_numbers_from_word(0, num_of_bc, 1, form.word.data)
+            wc = magicmaker.rand_numbers_from_word(0, num_of_wc, 10, form.word.data)
+
+            bc_return = blackcards.retrieve_card(bc[0])
+            wc_return = []
+
+            counter = 0
+            for i in range(5):
+                temp = []
+                for j in range(2):
+                    temp.append(whitecards.retrieve_card(wc[counter]))
+                    counter = counter + 1
+                wc_return.append(temp)
+        return render_template("play.html", title="play", form=form, blackcard=bc_return, whitecards=wc_return)
+
+
+#FUNCTION
+#deletes black card
+@app.route("/delete_bc/<index>", methods=['POST', 'GET'])
+def delete_blackcard(index):
+    blackcards.delete_card(index)
+    flash ("Black card deleted", 'success')
+    return redirect(url_for('black_cards'))
+
+
+#FUNCTION
+#deletes white card
+@app.route("/delete_wc/<index>", methods=['POST', 'GET'])
+def delete_whitecard(index):
+    whitecards.delete_card(index)
+    flash ("White card deleted", 'success')
+    return redirect(url_for('white_cards'))
+
 
 #Rules
 @app.route("/rules", methods=['POST', 'GET'])
 def rules():
     return render_template("rules.html", title="rules")
-
-#play
-#TAKES A INPUT
-@app.route("/play", methods=['POST', 'GET'])
-def play():
-    form = LetsPlay()
-    #if form.validate_on_submit():
-
-    return render_template("play.html", title="play", form=form)
-
-
 
 
 #add black card
--- magicmaker.py
+++ magicmaker.py
@@ -2,7 +2,7 @@ import random
 import hashlib
 
 #generates random numebrs
-#INPUT: start of range, end of range, number of numbers to generate
+#INPUT: start of range, end of range, number of numbers to generate (default seed)
 def random_number_generator(start, end, num):
     random.seed()
     nums = []
@@ -10,10 +10,12 @@ def random_number_generator(start, end, num):
         nums.append(random.randint(start, end))
     return nums
 
-#generate numbers from string
-#INPUT: a given word
-def numbers_from_word(word):
+#same as above but uses a word as a seed
+#INPUT: start of range, end of range, number of numbers to generate, word to generate hash and seed
+def rand_numbers_from_word(start, end, num, word):
     hashed = int(hashlib.sha256(word.encode('UTF-8')).hexdigest(), base=16)
     random.seed(hashed)
-    nums = random_number_generator(0, len(word), len(word))
+    nums = []
+    for i in range(num):
+        nums.append(random.randint(start, end))
     return nums
